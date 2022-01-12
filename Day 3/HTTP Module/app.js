b01fd2d7ec38c9722a5fec65b127c4ab9def477c
Rouge Nij <rougenij@gmail.com>
2022-01-12 11:06:39 +0200

    Fixing the require

--- Day 3/HTTP Module/app.js
+++ Day 3/HTTP Module/app.js
@@ -1,7 +1,7 @@
-const http = require("http");
+const https = require("https");
 const url = "https://api.fbi.gov/wanted/v1/list";
 
-const request = http.request(url, (response) => {
+const request = https.request(url, (response) => {
   let data = "";
 
   response.on("data", (chunk) => {

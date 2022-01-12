0451ed5df6e5947253569fdf1aedc3537cbbb429
Rouge Nij <rougenij@gmail.com>
2022-01-12 15:57:58 +0200

    Purenode ex

--- Day 3/HTTP Module/purenode.js
+++ Day 3/HTTP Module/purenode.js
@@ -1,28 +1,51 @@
-// Import the HTTP module
 const http = require("http");
-// Import the URL module
-const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
-// Make our HTTP server
+const PORT = 3000;
+const users = [
+  {
+    name: "Reut",
+    email: "fake-email@domain.com",
+  },
+  {
+    name: "Jenny",
+    email: "fake-email2@domain.com",
+  },
+];
+
 const server = http.createServer((req, res) => {
-  // Parse the request url
-  const reqUrl = url.parse(req.url).pathname;
-  if (req.method == "GET") {
-    if (reqUrl == "/") {
-      res.write("<h1>Ayy welcome to my pure node website</h1>");
-      res.end();
-    }
-  } else if (req.method == "GET") {
-    if (reqUrl == "/users") {
-      res.write("Just imagine that there is a users JSON file here");
-      res.end();
-    }
-  } else if (req.method == "GET") {
-    if (reqUrl == "/raw-html”") {
-      res.write("<h1>Welcome</h1>");
-      res.end();
-    }
+  if (req.url === "/raw-html") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>welcome</h1>");
+    res.end();
+  }
+  if (req.url === "/users") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify(users));
+    res.end();
+  }
+  if (req.url === "/") {
+    const htmlPath = path.join(__dirname, "./public/index.html");
+
+    fs.readFile(htmlPath, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write("File not found!");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      }
+    });
   }
 });
-// Have the server listen on port 8080
-server.listen(8080);
+
+server.listen(PORT, (error) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  console.log(`Server listening on port ${PORT}`);
+});
